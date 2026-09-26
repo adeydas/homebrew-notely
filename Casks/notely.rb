77cfae993899de38f7649c0f648fc25b9a1139d4
Abhishek <abhishek@abhis.ws>
2026-09-26 00:09:00 -0700

    Publish Notely 8.5.0 (93)

--- Casks/notely.rb
+++ Casks/notely.rb
@@ -1,6 +1,6 @@
 cask "notely" do
-  version "8.4.0,92"
-  sha256 "c9bd002c740724174853cb6ddeef7acdb90e62219066a0e03cffb8f7dc29de25"
+  version "8.5.0,93"
+  sha256 "d05c500fdf483e8e1f237d8bdf7f66b42aa28caee09c86083a40ab8177e0b7b6"
 
   url "https://notelyapp.ca/downloads/releases/#{version.csv.first}/#{version.csv.second}/Notely.dmg"
   name "Notely"
